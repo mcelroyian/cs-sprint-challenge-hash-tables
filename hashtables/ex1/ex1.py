b0bf7b4971e38fe1b2dd0e46efbde13373778140
Ian McElroy <mcelroyian@gmail.com>
2020-08-07 11:17:11 -0500

    ex5

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,35 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    if length == 0:
+        return None
     # Your code here
-
-    return None
+    h = {}
+    r = []
+    #build hash
+    for i in range(length):
+        if weights[i] not in h:
+            h[weights[i]] = {"d": False, "value": i}
+        else:
+            h[weights[i]] = {"d": True, "value": i}
+    #loop through array checking if the diff num exists
+    #check if another key exists as diff.
+    for i in range(length):
+        print(weights[i])
+        diff = limit - i
+        if diff == i and h[i]["d"]:
+            r.append(i)
+            r.append(h[i]["value"])
+            print(r)
+            return tuple(r.sort())
+        if diff in h.keys():
+            r.append(h[i]["value"])
+            r.append(h[diff]["value"])
+            print(r)
+            return tuple(r.sort(reverse=True))
+        #if yes add the two indexes to result
+    #if reach end, result is None. return
+    print(h)
+    print(r)
+    if not r:
+        return None
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,5 +1,5 @@
 # Your code here
-
+import os
 
 
 def finder(files, queries):
@@ -7,7 +7,17 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
-
+    d = {}
+    result = []
+    for path in files:
+        file = os.path.basename(path)
+        if file not in d.keys():
+            d[file] = []
+        d[file].append(path)
+    for q in queries:
+        if q in d.keys():
+            for path in d[q]:
+                result.append(path)
     return result
 
 
