2f3eaf26509a2f31d20496b26821c38c678947fe
Ian McElroy <mcelroyian@gmail.com>
2020-08-07 12:42:54 -0500

    ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -6,31 +6,12 @@ def get_indices_of_item_weights(weights, length, limit):
         return None
     # Your code here
     h = {}
-    r = []
     #build hash
     for i in range(length):
-        if weights[i] not in h:
-            h[weights[i]] = {"d": False, "value": i}
-        else:
-            h[weights[i]] = {"d": True, "value": i}
-    #loop through array checking if the diff num exists
-    #check if another key exists as diff.
-    for i in range(length):
-        print(weights[i])
-        diff = limit - i
-        if diff == i and h[i]["d"]:
-            r.append(i)
-            r.append(h[i]["value"])
-            print(r)
-            return tuple(r.sort())
+        num = weights[i]
+        diff = limit - num
         if diff in h.keys():
-            r.append(h[i]["value"])
-            r.append(h[diff]["value"])
-            print(r)
-            return tuple(r.sort(reverse=True))
-        #if yes add the two indexes to result
-    #if reach end, result is None. return
-    print(h)
-    print(r)
-    if not r:
-        return None
+            return (i,h[diff])
+        h[num] = i
+    return None
+
